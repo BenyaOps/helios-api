8eaa14c4965db694c7a6b0f83d9aae63d2c7dc03
BenyaOps <benyaminmit147@gmail.com>
2025-02-19 09:19:25 -0500

    first commit

---
+++ database/query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(45) NOT NULL UNIQUE,
+    superior_id INT,
+    nivel INT UNSIGNED NOT NULL,
+    employees_quantity INT UNSIGNED NOT NULL,
+    ambassador VARCHAR(255),
+    FOREIGN KEY (superior_id) REFERENCES departments(id)
+);
