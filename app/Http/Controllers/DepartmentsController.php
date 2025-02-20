7e6169c3504e4f3286f580384322973030043a24
BenyaOps <benyaminmit147@gmail.com>
2025-02-20 14:08:47 -0500

    feat

--- app/Http/Controllers/DepartmentsController.php
+++ app/Http/Controllers/DepartmentsController.php
@@ -14,19 +14,22 @@ class DepartmentsController extends Controller
 
     public function list(Request $request)
     {
-        $page = $request->input('page', 1);
-        $count = $request->input('count', 10);
-        $offset = ($page - 1) * $count;
-
+        $page = $request->query('page', 1);
+        $itemsPerPage = $request->query('item_to_page', 10);
+        $offset = ($page - 1) * $itemsPerPage;
+        //var_dump($offset);die();
         $departments = Departments::join('ambassadors', 'departments.ambassador_id', '=', 'ambassadors.id')
             ->offset($offset)
-            ->limit($count)
+            ->limit($itemsPerPage)
             ->get(["departments.id as department_id", "departments.name as department_name",
              "superior_id","nivel","employees_quantity","ambassadors.name as ambassador_name"
         ]);
 
         $data = [];
         $data['departments'] = $departments;
+        $data['total'] = Departments::count();
+        $data['page'] = $page;
+        $data['itemsPerPage'] = $itemsPerPage;
         return  $this->response(200, 'SUCCESSFUL', $data);
     }
 }
